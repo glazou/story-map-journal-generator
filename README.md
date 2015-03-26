2feb309f9bde4fdeb4e7615b0115b70d41a0a35e
Gregory L'Azou <glazou@esri.com>
2015-03-25 22:05:27 -0700

    Initial release

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The Story Map Journal is ideal when you want to combine narrative text with maps and other embedded content. 
 
-The typical Map Journal creation process involve using a WYSIWYG builder. That repository let you generate a Map Journal through a simple JSON document, without using the builder.
+The typical Map Journal creation involve using the integrated WYSIWYG builder. That app let you generate a Map Journal through a simple JSON document, without using that builder.
 
 The resulting application is hosted in ArcGIS Online, you can edit it later as if you had used the builder.
 
@@ -11,9 +11,137 @@ The resulting application is hosted in ArcGIS Online, you can edit it later as i
 
 ## Instructions
 
-1. Download
-2. Edit app/app.js with your credentials
-3. Optionally if you want to use the `webmap-x-y` type, edit the webmap ID
-2. Deploy
+The usage scenarios are:
+* as a node.js application with ArcGIS Online credentials defined in the configuration file
+* as a web application with authentication through ArcGIS Online OAuth dialog
+* as a web application with authentication through ArcGIS Online credentials defined provided in the configuration file
 
-**Make sure to not deploy the application on a public server to avoid exposing your password.**
+### As a Node.js application
+
+1. Install [Node.js](http://nodejs.org/) 
+2. Download the application
+3. Run `npm install` in the application root
+4. Edit app/app.js with your credentials and an ArcGIS Online OAuth App ID, see [adding items for ArcGIS Online for Organizations users](http://doc.arcgis.com/en/arcgis-online/share-maps/add-items.htm#ESRI_SECTION1_0D1B620254F745AE84F394289F8AF44B) or [registering your application for ArcGIS for Developers users](https://developers.arcgis.com/authentication/signing-in-arcgis-online-users/#registering-your-application)
+5. Optionally if you want to use the `webmap-x-y` type, edit app/app.js with a webmap ID (`webmapForEmbed`)
+6. Run `node index`
+
+### As a web application with OAuth authentication
+
+1. Download the application
+2. Deploy on a web server
+4. Edit app/app.js with an ArcGIS Online OAuth App ID, see [adding items for ArcGIS Online for Organizations users](http://doc.arcgis.com/en/arcgis-online/share-maps/add-items.htm#ESRI_SECTION1_0D1B620254F745AE84F394289F8AF44B) or [registering your application for ArcGIS for Developers users](https://developers.arcgis.com/authentication/signing-in-arcgis-online-users/#registering-your-application)
+5. Optionally if you want to use the `webmap-x-y` type, edit app/app.js with a webmap ID (`webmapForEmbed`)
+6. Access the application over the web server
+
+### As a web application with ArcGIS Online credentials
+
+1. Install [Node.js](http://nodejs.org/) 
+2. Download the application
+3. Run `npm install` in the application root
+4. Edit app/app.js with your credentials
+5. Optionally if you want to use the `webmap-x-y` type, edit app/app.js with a webmap ID (`webmapForEmbed`)
+6. Run `node index`
+
+**Make sure to not deploy the application on a public server to avoid exposing your credentials.**
+
+## JSON configuration
+
+The JSON defining the Map Journal to be generated can be edited through `data/map-journal-sample.json`.
+
+The general JSON structure is:
+```
+{
+	title: 'Journal title',
+	sections: [
+		{
+			// First
+		},
+		{
+			// Second section
+		},
+		// ... limited to 99 sections ... 
+	]
+}
+```
+
+With a section being defined by a title a `Main Stage` (larger area) and `Panel` (smaller scollable area intended for the narrative).
+
+```
+{
+	"title": "",
+	"mainstage": {
+		// Image, video, webpage or map configure goes here
+	},
+	"panel": {
+		// Text, can include images, videos, webpage and map 
+	}
+}
+```
+
+See some exemple below.
+
+**Section with an image in the Main Stage**
+
+```
+{
+	"title": "<p>First section</p>",
+	"mainstage": {
+		"type": "image",
+		"url": "http://farm8.static.flickr.com/7373/13429795365_f57595be16_b.jpg"
+	},
+	"panel": {
+		"text": "<p>Image in main stage</p>"
+	}
+}
+```
+
+**Section with a Map in the Main Stage**
+ ```
+{
+	"title": "<p>Second section</p>",
+	"mainstage": {
+		"type": "webmap",
+		"id": "ea8e4e1f70014f3a8919dc3d8c08a29e"
+	},
+	"panel": {
+		"text": "<p>A Map of <strong>world volcanoes</strong>.</p><p>&nbsp;</p><p>&nbsp;</p><p>Open the Map in <a href=\"http://www.arcgis.com/home/webmap/viewer.html?webmap=2a45f767a6e549bfbe5bf638681f1fac\" target=\"_blank\">ArcGIS Online Map Viewer</a>.<p>&nbsp;</p><p>&nbsp;</p></p>"
+	}
+}
+```
+
+**Section with a webpage in the Main Stage and an image in the panel**
+ ```
+{
+	"title": "<p>Third section</p>",
+	"mainstage": {
+		"type": "webpage",
+		"url": "http://story.maps.arcgis.com/home/webscene/viewer.html?webscene=fd3b56984801434aa71f75b1926bde5b&ui=min"
+	},
+	"panel": {
+		"text": "<p>Embed of ArcGIS Online scene viewer, <a href=\"http://story.maps.arcgis.com/home/webscene/viewer.html?webscene=fd3b56984801434aa71f75b1926bde5b\" target=\"_blank\">link to the scene</a>.</p><p><div class=\"image-container activate-fullscreen\"><img src=\"http://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Fimmvorduhals_2010_03_27_dawn.jpg/1280px-Fimmvorduhals_2010_03_27_dawn.jpg\" /></div></p>"
+	}
+}
+```
+
+**Section with a Map in the Main Stage and in the panel configured as an x/y**
+ ```
+{
+	"title": "<p>Fourth <strong style=\"color:red\">section</strong></p>",
+	"mainstage": {
+		"type": "map-x-y",
+		"x": -116.538019,
+		"y": 33.825898,
+		"mapScale": "block",
+		"mapSymbolUrl": "http://static.arcgis.com/images/Symbols/Shapes/PurplePin2LargeB.png",
+		"popupContents": "Hello Palm Springs!"
+	},
+	"panel": {
+		"text": "<p>Map in the main stage <strong>+ map in the panel</strong></p>",
+		"map": {
+			"x": -116.538019,
+			"y": 33.825898,
+			"mapScale": "continent"
+		}
+	}
+}
+```
---
+++ app/app.js
@@ -0,0 +1,723 @@
+/**
+ * This application is released under the Apache License V2.0
+ * Checkout the project repository on GitHub: https://github.com/glazou/story-map-journal-generator
+ */
+
+/*
+ * Configuration
+ */
+
+var ARCGIS_INFO = {
+	authentication: {
+		type: 'oauth', // 'oauth' | 'user'
+		oauth: {
+			appid: '',
+			jsapi: '//js.arcgis.com/3.13/'
+		},
+		user: {
+			user: '',
+			pwd:  '',
+		}
+	},
+	url:  'www.arcgis.com/',
+	// To be edited if 'map-x-y' is used
+	webmapForEmbed: 'd6bb04eaaa714482896f3b50a0730e4e'
+};
+
+/*
+ * Constants
+ */
+
+var JOURNAL_MAIN_STAGE_TYPE = ['webmap', 'image', 'video', 'webpage', 'map-x-y'];
+var MAP_LEVELS = {
+	world: 1,
+	continent: 3,
+	country: 5,
+	state: 6,
+	county: 10,
+	city:13,
+	block: 16
+};
+
+var isBrowser = typeof window !== 'undefined';
+var itemTemplate = {
+	item: null,
+	itemData: null
+};
+
+if ( ARCGIS_INFO.authentication.type == 'user' && (! ARCGIS_INFO.authentication.user.user || ! ARCGIS_INFO.authentication.user.pwd) ) {
+	error("Username and password not configured: see app/app.js");
+
+	if (isBrowser) {
+		$("#generate-map-journal").addClass("disabled");
+		$("#json-data").val("");
+	}
+}
+else if (isBrowser) {
+	if ( ARCGIS_INFO.authentication.type == 'oauth' ) {
+		if ( ARCGIS_INFO.authentication.oauth.appid ) {
+			document.write("<script language='javascript' type='text/javascript' src='" + ARCGIS_INFO.authentication.oauth.jsapi + "'><\/script>");
+			document.write("<script language='javascript' type='text/javascript'>require([\"esri/IdentityManager\", \"dojo/domReady!\"], function(Map) { window.jsapiLoadedCallback(); });<\/script>");
+		}
+		else {
+			error("OAuth appid not configured");
+			$("#generate-map-journal").addClass("disabled");
+			$("#json-data").val("");
+		}
+	}
+	else {
+		$(document).ready(initView);
+	}	
+}
+// Node.js
+else {
+	var path = require('path');
+	var fs = require('fs');
+	var deferred = require('deferred');
+	var $ = require('jquery');
+    var rp = require('request-promise');
+
+    getItemTemplate();
+
+	var filePath1 = path.join(__dirname, '../data/map-journal-sample.json');
+	var journalJson = fs.readFileSync(filePath1, { encoding: 'utf-8' });
+
+	generateMapJournal(journalJson);
+}
+
+function generateMapJournal(rawData)
+{
+	parseJournalDefinition(rawData).then(function(data){
+		createMapJournal(data);
+	});
+}
+
+/*
+ * Journal data parsing
+ */
+
+function parseJournalDefinition(rawData)
+{
+	var resultDeferred = getDeferred();
+
+	if ( ! itemTemplate.item || ! itemTemplate.itemData ) {
+		error("Could not load Map Journal item template");
+		return;
+	}
+
+	if ( isBrowser ) { 
+		generateStep1();
+	}
+
+	if( ! rawData ) {
+		error("No data");
+		return;
+	}
+
+	var journal = null;
+
+	try {
+		journal = JSON.parse(rawData);
+	} catch(e) {
+		console.error(e);
+	}
+
+	if ( ! journal ) {
+		error("Could not parse the JSON");
+		return;
+	}
+
+	if ( ! journal.title ) {
+		error("Journal title not defined");
+		return;
+	}
+
+	if ( ! journal.sections || ! journal.sections.length ) {
+		error("Sections not defined");
+		return;
+	}
+
+	journal.sections.forEach(function(section,i){
+		var invalid = false;
+
+		if ( ! section.title ){
+			error("Section " + i + " is missing title");
+			invalid = true;
+		}
+
+		if ( ! section.mainstage ){
+			error("Section " + i + " is missing main stage");
+			invalid = true;
+		}
+
+		if ( ! section.panel ){
+			error("Section " + i + " is missing panel");
+			invalid = true;
+		}
+
+		if ( JOURNAL_MAIN_STAGE_TYPE.indexOf(section.mainstage.type) == -1 ){
+			error("Section " + i + " mainstage is not valid");
+			invalid = true;
+		}
+
+		if ( invalid ) {
+			return;
+		}
+
+		section.content = section.panel.text;
+		section.media = {};
+
+		if( section.panel.map ) {
+			if ( ! section.panel.map.x || ! section.panel.map.y ) {
+				error("Section " + i + " panel map is not valid");
+			}
+			else {
+				section.content += '<p><div class="iframe-container mj-frame-by-frametag fit" tabindex="0">' + buildMapEmbedFrame({
+					x: section.panel.map.x,
+					y: section.panel.map.y,
+					scale: section.panel.map.mapScale,
+					popup: {
+						symbolUrl: section.panel.map.mapSymbolUrl,
+						content: section.panel.map.popupContents
+					}
+				}) + '</div></p><br />';
+			}
+		}
+
+		if( section.mainstage.type == 'map-x-y' ) {
+			var ts = null;
+
+			if (typeof window !== 'undefined') {
+				ts = performance.now(); // Date.now()
+			}
+			else {
+				var hrTime = process.hrtime();
+				ts = hrTime[0] * 1000000 + hrTime[1] / 1000;
+			}
+
+			section.media = {
+				type: 'webpage',
+				webpage: {
+					display: 'stretch',
+					ts: ts, 
+					url: "",
+					frameTag: buildMapEmbedFrame({
+						x: section.mainstage.x,
+						y: section.mainstage.y,
+						scale: section.mainstage.mapScale,
+						popup: {
+							symbolUrl: section.mainstage.mapSymbolUrl,
+							content: section.mainstage.popupContents
+						},
+						unload: true
+					})
+				}
+			};
+		}
+		else if( section.mainstage.type == 'webmap' ) {
+			section.media.type = section.mainstage.type;
+			section.media[section.mainstage.type] = {
+				id: section.mainstage.id,
+				extent: null,
+				layers: null,
+				popup: null,
+				legend: {
+					enable: false,
+					openByDefault: false
+				},
+				overview: {
+					enable: false,
+					openByDefault: false
+				}
+			};
+		}
+		else {
+			section.media.type = section.mainstage.type;
+
+			var display = section.mainstage.display;
+
+			if ( ! display ) {
+				display = section.media.type == 'image' ? 'fill' : 'stretch';
+			}
+
+			section.media[section.mainstage.type] = {
+				display: display,
+				url: section.mainstage.url
+			};
+		}
+
+		delete section.mainstage;
+		delete section.panel;
+
+		section.contentActions = [];
+		section.creaDate = Date.now();
+		section.pubDate = Date.now();
+		section.status = "PUBLISHED";		
+	});
+
+	if ( isBrowser ) {
+		console.log("Sections parsed successfully:", journal.sections);
+	}
+	else {
+		console.log("Sections parsed successfully");
+	}
+
+	if ( ! journal.sections.length ) {
+		error("Could not find any section");
+		return;
+	}
+
+	if ( isBrowser ) {
+		$("#json-data").attr("disabled", true);
+	}
+
+	resultDeferred.resolve(journal);
+
+	return isBrowser ? resultDeferred : resultDeferred.promise;
+}
+
+function createMapJournal(journal)
+{
+	var user = ARCGIS_INFO.authentication.oauth.user || ARCGIS_INFO.authentication.user.user;
+
+	/*
+	 * Get ArcGIS Online Token
+	 */
+
+	getToken().then(function(token){
+		console.log("ArcGIS Online token: OK");
+
+		/*
+		 * Create Map Journal item
+		 */
+
+		var item = itemTemplate.item,
+			itemData = itemTemplate.itemData;
+
+		item.title = journal.title;
+
+		itemData.values.story.sections = journal.sections;
+
+		createItem(
+			user,
+			token,
+			itemTemplate.item,
+			itemTemplate.itemData
+		).then(function(itemId){
+			console.log("Create item: OK");
+
+			/*
+			 * Update Map Journal item
+			 */
+
+			updateItemData(
+				user,
+				token,
+				itemId,
+				{
+					url: getMapJournalUrl(itemId)
+				}
+			).then(function(success){
+				console.log("Update item: OK");
+
+				/*
+				 * Share Map Journal item
+				 */
+
+				shareItem(
+					user,
+					token,
+					itemId
+				).then(function(success){
+					console.log("Share item: OK");
+
+					if ( success ) {
+						if ( isBrowser ) {
+							$("#generate-map-journal")
+								.html('Story generated!');
+
+							$(".progress").addClass("hide");
+
+							setTimeout(function(){
+								$("#generate-map-journal")
+									.removeClass('disabled')
+									.html('Generate the Map Journal');
+							}, 2000);
+
+							$("#open-map-journal")
+								.removeClass('hide')
+								.html('Open the Map Journal')
+								.off('click')
+								.on('click', function(){
+									openMapJournal(itemId);
+								});
+
+
+							$("#json-data").attr("disabled", false);
+						}
+						else {
+							console.log("Journal generated successfully");
+							console.log(getMapJournalUrl(itemId));
+						}
+					}
+				});
+			});
+		});
+	});
+}
+
+/*
+ * Utils
+ */
+
+function buildMapEmbedFrame(p)
+{
+	var url = 'http://www.arcgis.com/apps/Embed/index.html' +
+		'?webmap=' + ARCGIS_INFO.webmapForEmbed +
+		'&center=' + p.x + ',' + p.y +
+		'&scale=false';
+
+	/*
+	 * Scale
+	 */
+	var scale = null;
+
+	if ( p.scale ) {
+		if ( MAP_LEVELS[p.scale] !== undefined ){
+			scale = MAP_LEVELS[p.scale];
+		}
+	}
+
+	if ( scale === null ) {
+		scale = MAP_LEVELS.county;
+	}
+
+	url += '&level=' + scale;
+
+	/*
+	 * Pop-up
+	 */
+	if ( p.popup ) {
+		if ( ! p.popup.symbolUrl )
+			p.popup.symbolUrl = 'http%3A%2F%2Fstatic.arcgis.com%2Fimages%2FSymbols%2FShapes%2FBluePin2LargeB.png';
+		else
+			p.popup.symbolUrl = encodeURIComponent(p.popup.symbolUrl);
+
+		url += '&marker=' + p.x + ';' + p.y + ';;;' + p.popup.symbolUrl + ';';
+
+		if ( p.popup.content )
+			url += p.popup.content;
+	}
+
+	return '<iframe width="100%" height="225px" style=" border: none;" src="' + url + '"></iframe>';
+}
+
+function getMapJournalUrl(itemId)
+{
+	return 'http://' + ARCGIS_INFO.url + 'apps/MapJournal/?appid=' + itemId;
+}
+
+/*
+ * Item template
+ */
+
+function getItemTemplate()
+{
+	if ( isBrowser ) {
+		$.ajax({ 
+			type: 'GET',
+			url: 'data/map-journal-item-template.json',
+			dataType: 'json'
+		}).then(function(data){
+			itemTemplate.item = data;
+		});
+
+		$.ajax({ 
+			type: 'GET',
+			url: 'data/map-journal-item-data-template.json',
+			dataType: 'json'
+		}).then(function(data){
+			itemTemplate.itemData = data;
+		});
+	}
+	else {
+		var filePath2 = path.join(__dirname, '../data/map-journal-item-template.json');
+		itemTemplate.item = JSON.parse(fs.readFileSync(filePath2, { encoding: 'utf-8' }));
+
+		var filePath3 = path.join(__dirname, '../data/map-journal-item-data-template.json');
+		itemTemplate.itemData = JSON.parse(fs.readFileSync(filePath3, { encoding: 'utf-8' }));
+	}
+}
+
+/*
+ * ArcGIS Online utils
+ */
+
+function getToken()
+{
+	var resultDeferred = getDeferred();
+
+	if ( ! isBrowser && ARCGIS_INFO.authentication.type == 'oauth' ) {
+		error("Can't use OAuth when using node.js");
+		resultDeferred.reject();
+	}
+	else if ( ARCGIS_INFO.authentication.type == 'oauth' ) {
+		resultDeferred.resolve(ARCGIS_INFO.authentication.oauth.token);
+	}
+	else {
+		agolRequest(
+			"POST",
+			"generateToken", 
+			null,
+			{
+				username: ARCGIS_INFO.authentication.user.user,
+				password: ARCGIS_INFO.authentication.user.pwd,
+				referer: isBrowser ? document.location.origin : 'http://www.arcgis.com',
+				f: 'json'
+			}
+		).then(function(rawResponse){
+			var response = JSON.parse(rawResponse);
+			if ( response && response.token )
+				resultDeferred.resolve(response.token);
+		});
+	}
+
+	return isBrowser ? resultDeferred : resultDeferred.promise;
+}
+
+function createItem(user, token, _item, itemData)
+{
+	var resultDeferred = getDeferred();
+
+	//var item = $.extend({}, _item);
+	var item = JSON.parse(JSON.stringify(_item));
+
+	item.uploaded = Date.now();
+	item.modified = Date.now();
+	item.owner = user;
+	item.token = token;
+	item.f = 'json';
+	item.overwrite = true;
+	item.text = JSON.stringify(itemData);
+
+	item.tags = item.tags.join(',');
+	item.typeKeywords = item.typeKeywords.join(',');
+
+	agolRequest(
+		"POST",
+		"addItem", 
+		user,
+		item
+	).then(function(rawResponse){
+		var response = JSON.parse(rawResponse);
+		if ( response && response.success )
+			resultDeferred.resolve(response.id);
+	});
+
+	return isBrowser ? resultDeferred : resultDeferred.promise;
+}
+
+function updateItemData(user, token, itemId, itemData)
+{
+	var resultDeferred = getDeferred();
+
+	itemData.f = 'json';
+	itemData.token = token;
+
+	agolRequest(
+		"POST",
+		"update", 
+		user + '/items/' + itemId,
+		itemData
+	).then(function(rawResponse){
+		var response = JSON.parse(rawResponse);
+		if ( response && response.success )
+			resultDeferred.resolve(response.id);
+	});
+
+	return isBrowser ? resultDeferred : resultDeferred.promise;
+}
+
+function shareItem(user, token, id)
+{
+	var resultDeferred = getDeferred();
+
+	agolRequest(
+		"POST",
+		"shareItems", 
+		user,
+		{
+			everyone: true,
+			items: id,
+			f: 'json',
+			token: token
+		}
+	).then(function(rawResponse){
+		var response = JSON.parse(rawResponse);
+		
+		if ( response && response.results && response.results.length )
+			resultDeferred.resolve(response.results[0].success);
+	});
+
+	return isBrowser ? resultDeferred : resultDeferred.promise;
+}
+
+function agolRequest(type, operation, resource, data)
+{
+	var url = 'https://' + ARCGIS_INFO.url + 'sharing/rest/';
+	
+	if ( resource )
+		url += 'content/users/' + resource + '/';
+
+	url += operation;
+
+	return doRequest(type, url, data);
+}
+
+/*
+ * Browser / node.js abstraction
+ */
+
+function getDeferred()
+{
+	if (typeof window !== 'undefined') {
+		return new $.Deferred();
+	}
+	else {
+		return deferred();
+	}
+}
+
+function doRequest(method, url, data)
+{
+	//console.log(method, url, data);
+
+	if ( isBrowser ) {
+		return $.ajax(
+			{
+				type: method,
+				url: url,
+				data: data
+			} 
+		);
+	}
+	else {
+		return rp(
+			{
+				method: method,
+				uri: url,
+				qs: data
+			}
+		);
+	}
+}
+
+/*
+ * UI
+ */
+
+function jsapiLoadedCallback()
+{
+	require(["esri/IdentityManager", "esri/arcgis/OAuthInfo"], function(IdentityManager, ArcGISOAuthInfo)
+	{
+		var info = new ArcGISOAuthInfo({
+			appId: ARCGIS_INFO.authentication.oauth.appid,
+			popup: false
+		});
+
+		IdentityManager.registerOAuthInfos([info]);
+
+		IdentityManager.checkSignInStatus(info.portalUrl).then(
+			function(authInfo) {
+				ARCGIS_INFO.authentication.oauth.user = authInfo.userId;
+				ARCGIS_INFO.authentication.oauth.token = authInfo.token;
+				initView();
+			}, 
+			function() {
+				IdentityManager.getCredential(info.portalUrl);
+			}
+		);
+	});
+}
+
+function initView()
+{	
+	$.ajax({ 
+		type: 'GET',
+		url: 'data/map-journal-sample.json',
+		dataType: 'json'
+	}).then(function(data){
+		$("#json-data").val(JSON.stringify(data, null, 2));
+		
+		$("#generate-map-journal")
+			.click(function(){
+				generateMapJournal($("#json-data").val());
+			})
+			.removeClass('hide')
+			.html('Generate the Map Journal');
+
+		$("#json-data").keyup(function(){
+			$("#generate-map-journal")
+				.toggleClass("disabled", ! $("#json-data").val())
+				.html('Generate the Map Journal');
+			$("#open-map-journal").addClass("hide");
+		});
+
+		$("#help-btn").click(function(){
+			$("#help-container").toggleClass("hide");
+		});
+
+	});
+
+	getItemTemplate();
+}
+
+function generateStep1()
+{
+	if (typeof window !== 'undefined') {
+		$("#generate-map-journal").addClass('disabled').html('Generating...');
+		$("#open-map-journal").addClass("hide");
+
+		startProgressIndicator();
+	}
+}
+
+function startProgressIndicator()
+{
+	var progress = 0;
+
+	$(".progress")
+		.removeClass("hide")
+		.find('.progress-bar').css("width", progress);
+
+	var handler = setInterval(function(){
+		progress += 7;
+
+		if ( progress <= 110 )
+			$(".progress-bar").css("width", progress + '%');
+		else
+			clearInterval(handler);
+	}, 150);
+}
+
+function error(errorLbl)
+{
+	console.error("Fatal error: " + errorLbl);
+
+	if ( isBrowser ) {
+		$(".progress").addClass("hide");
+
+		$("#errormsgcontainer")
+			.removeClass("hide")
+			.find(".errormsg").html("Error: " + errorLbl);
+		
+		$("#json-data").attr("disabled", false);
+		$("#generate-map-journal").removeClass('hide').html('Generate the Map Journal');
+		$("#open-map-journal").addClass("hide");
+	}
+}
+
+function openMapJournal(itemId)
+{
+	window.open(getMapJournalUrl(itemId), '_blank');
+}
